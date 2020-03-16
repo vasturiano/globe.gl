4cbc589430d90124819a37676dbd6688f5d6584f
vasco <vastur@gmail.com>
2020-03-16 03:18:28 -0700

    Refactor rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,61 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import { name, homepage, version } from './package.json';
+import { terser } from "rollup-plugin-terser";
+import dts from 'rollup-plugin-dts';
+import { name, homepage, version, dependencies } from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      format: 'umd',
-      name: 'Globe',
-      file: `dist/${name}.js`,
-      sourcemap: true,
-      banner: `// Version ${version} ${name} - ${homepage}`
-    }
-  ],
-  plugins: [
-    resolve(),
-    commonJs(),
-    babel({ exclude: 'node_modules/**' })
-  ]
+const umdConf = {
+  format: 'umd',
+  name: 'Globe',
+  banner: `// Version ${version} ${name} - ${homepage}`
 };
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: `dist/${name}.js`,
+        sourcemap: true
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonJs(),
+      babel({ exclude: 'node_modules/**' })
+    ]
+  },
+  { // commonJs and ES modules
+    input: 'src/index.js',
+    output: [
+      {
+        format: 'cjs',
+        file: `dist/${name}.common.js`
+      },
+      {
+        format: 'es',
+        file: `dist/${name}.module.js`
+      }
+    ],
+    external: Object.keys(dependencies),
+    plugins: [
+      babel()
+    ]
+  },
+  { // expose TS declarations
+    input: 'src/index.d.ts',
+    output: [{
+      file: `dist/${name}.d.ts`,
+      format: 'es'
+    }],
+    plugins: [dts()]
+  }
+];
